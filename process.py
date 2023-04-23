c100ae323b76f539864036fd04f9915e379714a6
helioproduct <helioproduct.sync@gmail.com>
2023-04-23 18:39:50 +0300

    linux process class completed

--- process.py
+++ process.py
@@ -1,5 +1,8 @@
 import subprocess
+import psutil
+import time
 from enum import Enum
+from typing import List
 
 
 class ProcessStatus(Enum):
@@ -9,29 +12,38 @@ class ProcessStatus(Enum):
 
 
 class LinuxProcess:
-    def __init__(self, command):
-        self.command = command
-        self.process = None
-        self.status = ProcessStatus.NOT_STARTED
+    def __init__(self, command: List[str]):
+        self._start_time = None
+        self._end_time = None
+        self._command = command
+        self._status = ProcessStatus.NOT_STARTED
+        self._process = None
 
     def start(self):
         if self.status != ProcessStatus.RUNNING:
-            self.process = subprocess.Popen(self.command)
-        elif self.process:
-            self.status = ProcessStatus.RUNNING
-        else:
-            self.status = ProcessStatus.NOT_STARTED
+            self._process = subprocess.Popen(self._command, stdout=subprocess.PIPE)
+            self._start_time = time.time()
+        if self._process:
+            self._status = ProcessStatus.RUNNING
 
     def stop(self):
         if self.status == ProcessStatus.RUNNING:
-            self.process.kill()
-        self.process = None
+            self._process.kill()
+            self._end_time = time.time()
+            self._status = ProcessStatus.KILLED
 
-    def get_status(self):
-        return self.status
+    @property
+    def status(self):
+        return self._status
+
+    @property
+    def pid(self):
+        if self.status == ProcessStatus.NOT_STARTED:
+            return -1
+        return self._process.pid
 
     def get_result(self):
-        if self.process:
+        if self.status == ProcessStatus.RUNNING:
             return 'Process is still running'
         return subprocess.check_output(self.command, shell=True).decode("utf-8")
 
